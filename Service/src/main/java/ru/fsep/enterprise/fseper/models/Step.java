dc9e2319d86680381bbe094804cf12945053bd75
rama333 <ramil-faxrut@yandex.ru>
2015-07-06 18:50:57 +0400

    new versions 1.00

--- Service/src/main/java/ru/fsep/enterprise/fseper/models/Step.java
+++ Service/src/main/java/ru/fsep/enterprise/fseper/models/Step.java
@@ -1,5 +1,8 @@
 package ru.fsep.enterprise.fseper.models;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
 /**
  * 05.07.15
  * Step
@@ -13,5 +16,42 @@ public class Step {
     private int task_id;
     private String description;
     private boolean finished;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("task_id", task_id)
+                .add("description", description)
+                .add("finished", finished)
+                .toString();
+    }
+
+    public Step(int id, int task_id, String description, boolean finished) {
+        this.id = id;
+        this.task_id = task_id;
+        this.description = description;
+        this.finished = finished;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, task_id, description, finished);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Step other = (Step) obj;
+        return Objects.equal(this.id, other.id)
+                && Objects.equal(this.task_id, other.task_id)
+                && Objects.equal(this.description, other.description)
+                && Objects.equal(this.finished, other.finished);
+    }
     // TODO: make body by wiki
 }
--- Service/src/main/java/ru/fsep/enterprise/fseper/models/Task.java
+++ Service/src/main/java/ru/fsep/enterprise/fseper/models/Task.java
@@ -1,5 +1,8 @@
 package ru.fsep.enterprise.fseper.models;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +17,50 @@ import java.util.List;
 public class Task {
     private int id;
     private boolean privated;
+
+    public Task(int id, boolean privated, String description, Date dueDate, List<Step> steps, boolean finished) {
+        this.id = id;
+        this.privated = privated;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.steps = steps;
+        this.finished = finished;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("privated", privated)
+                .add("description", description)
+                .add("dueDate", dueDate)
+                .add("steps", steps)
+                .add("finished", finished)
+                .toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, privated, description, dueDate, steps, finished);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Task other = (Task) obj;
+        return Objects.equal(this.id, other.id)
+                && Objects.equal(this.privated, other.privated)
+                && Objects.equal(this.description, other.description)
+                && Objects.equal(this.dueDate, other.dueDate)
+                && Objects.equal(this.steps, other.steps)
+                && Objects.equal(this.finished, other.finished);
+    }
+
     private String description;
     private Date dueDate;
     private List<Step> steps;
