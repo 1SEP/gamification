b47ed346b8a722b471517d7972419b2e3254628f
Rauf <goodraff@gmail.com>
2015-07-07 12:40:06 +0300

    modificate tables in database

--- SQL/db.sql
+++ SQL/db.sql
@@ -1,53 +1,44 @@
-CREATE TABLE 'users' (
-  'user_id' INT NOT NULL AUTO_INCREMENT,
-  'login' VARCHAR,
-  'info_id' INT,
-  'task_id' INT,
-  PRIMARY KEY  ('user_id')
-);
- 
-CREATE TABLE 'auth_data' (
-  'login' VARCHAR,
-  'password_hash' VARCHAR
-);
- 
-CREATE TABLE 'person_info' (
-  'info_id' INT,
-  'first_name' VARCHAR,
-  'last_name' VARCHAR,
-  'rating' DOUBLE,
-  'bithday' DATE,
-  'post_id' INT,
-  'role' VARCHAR,
-  'photo' VARCHAR
+CREATE TABLE auth_data (
+  login VARCHAR PRIMARY KEY,
+  password_hash VARCHAR
 );
 
-CREATE TABLE 'post' (
-  'post_id' INT,
-  'name' VARCHAR,
-  'description' VARCHAR
+CREATE TABLE person_info (
+  info_id INT PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  rating DOUBLE PRECISION,
+  bithday DATE,
+  post_id INT,
+  role VARCHAR,
+  photo VARCHAR
+) ;
+
+CREATE TABLE post (
+  post_id INT PRIMARY KEY,
+  name VARCHAR,
+  description VARCHAR
+) ;
+
+CREATE TABLE task (
+  task_id INT PRIMARY KEY ,
+  privated BOOLEAN,
+  description VARCHAR,
+  due_data DATE,
+  step_id INT,
+  finished BOOLEAN 
+) ;
+
+CREATE TABLE step (
+  step_id INT PRIMARY KEY ,
+  task_id INT REFERENCES task,
+  description VARCHAR,
+  finished BOOLEAN
+) ;
+
+CREATE TABLE users (
+  user_id INT PRIMARY KEY ,
+  login VARCHAR REFERENCES auth_data,
+  info_id INT REFERENCES person_info,
+  task_id INT REFERENCES task
 );
-
-CREATE TABLE 'task' (
-  'task_id' INT,
-  'privated' BOOLEAN,
-  'description' VARCHAR,
-  'due_data' DATE,
-  'step_id' INT,
-  'finished' BOOLEAN 
-);
-
-CREATE TABLE 'step' (
-  'step_id' INT,
-  'task_id' INT,
-  'description' VARCHAR,
-  'finished' BOOLEAN
-);
-
-
- 
- 
-ALTER TABLE 'auth_data' ADD CONSTRAINT 'auth_fk1' FOREIGN KEY ('login') REFERENCES users('login');
-ALTER TABLE 'person_info' ADD CONSTRAINT `info_fk1` FOREIGN KEY ('info_id') REFERENCES users('info_id');
-ALTER TABLE 'task' ADD CONSTRAINT 'task_fk1' FOREIGN KEY ('task_id') REFERENCES users('task_id');
-ALTER TABLE 'step' ADD CONSTRAINT 'step_fk1' FOREIGN KEY ('step_id') REFERENCES task('step_id');
