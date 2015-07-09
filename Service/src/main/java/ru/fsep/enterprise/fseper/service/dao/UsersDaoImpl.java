df64043876d3c082ffc975246b8f27090a283606
ildarJava <ildar.almakayev@gmail.com>
2015-07-09 16:47:14 +0400

    Was added the class UserDaoImpl

---
+++ Service/src/main/java/ru/fsep/enterprise/fseper/service/dao/UsersDaoImpl.java
@@ -0,0 +1,43 @@
+package ru.fsep.enterprise.fseper.service.dao;
+
+import ru.fsep.enterprise.fseper.models.User;
+
+import java.util.List;
+
+public class UsersDaoImpl implements UsersDao {
+    public void logIn(User user) {
+
+    }
+
+    public User getUser(int userId) {
+        return null;
+    }
+
+    public User updateUser(int userId) {
+        return null;
+    }
+
+    public void removeUser(int userId) {
+
+    }
+
+    public List<User> getUsers() {
+        return null;
+    }
+
+    public List<User> getUsersByName() {
+        return null;
+    }
+
+    public List<User> getUsersByPost() {
+        return null;
+    }
+
+    public List<User> getSortedUsers() {
+        return null;
+    }
+
+    public List<User> getSortedUsersByRating() {
+        return null;
+    }
+}
