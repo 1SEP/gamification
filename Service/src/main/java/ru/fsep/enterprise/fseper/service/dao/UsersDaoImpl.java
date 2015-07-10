d50708c3e95449d52c13bc72ac17664eb4027d8b
ildarJava <ildar.almakayev@gmail.com>
2015-07-10 09:30:33 +0400

    Added the dependencies for spring framework and postgresql

--- Service/src/main/java/ru/fsep/enterprise/fseper/service/dao/UsersDaoImpl.java
+++ Service/src/main/java/ru/fsep/enterprise/fseper/service/dao/UsersDaoImpl.java
@@ -1,10 +1,17 @@
 package ru.fsep.enterprise.fseper.service.dao;
 
 import ru.fsep.enterprise.fseper.models.User;
+import ru.fsep.enterprise.fseper.service.jdbc.utils.DaoArgumentsVerifier;
+import ru.fsep.enterprise.fseper.service.jdbc.utils.ParamsMapper;
+import ru.fsep.enterprise.fseper.service.jdbc.utils.SqlQueryExecutor;
 
 import java.util.List;
 
 public class UsersDaoImpl implements UsersDao {
+    private SqlQueryExecutor sqlQueryExecutor;
+    private ParamsMapper paramsMapper;
+    private DaoArgumentsVerifier daoArgumentsVerifier;
+
     public void logIn(User user) {
 
     }
