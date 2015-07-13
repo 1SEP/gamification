22f1190a723a59f4321f90678a044ba899f16329
rama333 <ramil-faxrut@yandex.ru>
2015-07-13 10:44:52 +0400

    pull

--- Service/src/main/java/ru/fsep/enterprise/fseper/service/dao/TasksDaoImpl.java
+++ Service/src/main/java/ru/fsep/enterprise/fseper/service/dao/TasksDaoImpl.java
@@ -2,14 +2,26 @@ package ru.fsep.enterprise.fseper.service.dao;
 
 import ru.fsep.enterprise.fseper.models.Task;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by ramil on 09.07.2015.
  */
 public class TasksDaoImpl implements TasksDao {
+    public void updateTask(Task task) {
+
+    }
+
+    public List<Task> getTasksByDate(Date date) {
+        return null;
+    }
 
     public void assignmentTask(Task task) {
+
+    }
+
+    public void assignmentTask(Task task, int userId) {
 
     }
 
@@ -24,6 +36,18 @@ public class TasksDaoImpl implements TasksDao {
 
     public void removeTask(int taskId) {
 
+    }
+
+    public List<Task> getPrivatedTasks() {
+        return null;
+    }
+
+    public List<Task> getFinishedTasks() {
+        return null;
+    }
+
+    public List<Task> getTasksByDate() {
+        return null;
     }
 
     public List<Task> getPrivatedTaskList() {
