1793c33099d6abfbbd4528d213cb3445cc5cd44d
ildarJava <ildar.almakayev@gmail.com>
2015-07-16 13:19:42 +0400

    was added the new constructor with 2 arguments for firstName and lastName of user. Was realized the methodr verifyUser

--- Service/src/main/java/ru/fsep/enterprise/fseper/service/exceptions/UserNotFoundException.java
+++ Service/src/main/java/ru/fsep/enterprise/fseper/service/exceptions/UserNotFoundException.java
@@ -5,14 +5,18 @@ import com.google.common.base.MoreObjects;
 public class UserNotFoundException extends RuntimeException {
     private String message;
 
-    public UserNotFoundException(int userId){
-        this.message = "User with id <" + userId + "> is not found";
+    public UserNotFoundException(int userId) {
+        this.message = "User with id <" + userId + "> was not found";
+    }
+
+    public UserNotFoundException(String firstName, String lastName){
+        this.message = "User(s) with this name <" + firstName + "> and surname <" + lastName + "> was(were) not found";
     }
 
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
-                .add("message", message)
+                .add("MESSAGE:", message)
                 .toString();
     }
 
