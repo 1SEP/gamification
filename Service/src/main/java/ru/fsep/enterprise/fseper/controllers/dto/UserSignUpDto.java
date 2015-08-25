14fd0ec676da27f90db7f9a93619576742ae5111
Flyur <flyur.karimov@gmail.com>
2015-08-25 11:49:24 +0300

    delete old version of project. added a new branch

--- Service/src/main/java/ru/fsep/enterprise/fseper/controllers/dto/UserSignUpDto.java
+++
@@ -1,8 +0,0 @@
-package ru.fsep.enterprise.fseper.controllers.dto;
-
-/**
- * Author Fedorov on 21.07.2015
- */
-public class UserSignUpDto {
-
-}
--- Service/src/test/java/ru/fsep/enterprise/fseper/service/jdbc/utils/test-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users(password_hash, login, last_name, first_name, rating, birthday, user_role, photo)
-    VALUES ('ildar_pas', 'ildar_login', 'Almakayev', 'Ildar', 8.8, '1991-12-31', 'java-dev', NULL);
